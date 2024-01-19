ffdb816acb89093140dc3eeb6b57753fd7c14eaf
youzunzhi <youzunzhi@163.com>
2024-01-19 17:55:16 +1100

    my main

--- scripts/coop/main.sh
+++ scripts/coop/main.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # custom config
-DATA=/path/to/datasets
+DATA=data
 TRAINER=CoOp
 
-DATASET=$1
-CFG=$2  # config file
-CTP=$3  # class token position (end or middle)
-NCTX=$4  # number of context tokens
-SHOTS=$5  # number of shots (1, 2, 4, 8, 16)
-CSC=$6  # class-specific context (False or True)
+DATASET=OxfordPets
+CFG=vit_b32 # config file
+CTP=end  # class token position (end or middle)
+NCTX=16  # number of context tokens
+SHOTS=16  # number of shots (1, 2, 4, 8, 16)
+CSC=False  # class-specific context (False or True)
 
 for SEED in 1 2 3
 do
@@ -17,8 +17,9 @@ do
     if [ -d "$DIR" ]; then
         echo "Oops! The results exist at ${DIR} (so skip this job)"
     else
-        python train.py \
-        --root ${DATA} \
+#        python train.py \
+        echo
+        "--root ${DATA} \
         --seed ${SEED} \
         --trainer ${TRAINER} \
         --dataset-config-file configs/datasets/${DATASET}.yaml \
@@ -27,6 +28,6 @@ do
         TRAINER.COOP.N_CTX ${NCTX} \
         TRAINER.COOP.CSC ${CSC} \
         TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \
-        DATASET.NUM_SHOTS ${SHOTS}
+        DATASET.NUM_SHOTS ${SHOTS}"
     fi
 done
