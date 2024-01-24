6c6112234d338caab372bdf3e09083da030faa29
youzunzhi <youzunzhi@163.com>
2024-01-24 22:14:12 +1100

    quick debug sh

--- scripts/coop/main.sh
+++ scripts/coop/main.sh
@@ -19,6 +19,24 @@ do
     DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}_${GRID_N}x${GRID_N}/seed${SEED}
     if [ -d "$DIR" ]; then
         echo "Oops! The results exist at ${DIR} (so skip this job)"
+        echo "Remove this directory? (y/n)"
+        read answer
+        if [ "$answer" = "y" ]; then
+            rm -rf ${DIR}
+            python train.py \
+            --root ${DATA} \
+            --seed ${SEED} \
+            --trainer ${TRAINER} \
+            --dataset-config-file configs/datasets/${DATASET}.yaml \
+            --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+            --output-dir ${DIR} \
+            --grid_n ${GRID_N} \
+            --total_train_batches ${TOTAL_TRAIN_BATCHES} \
+            TRAINER.COOP.N_CTX ${NCTX} \
+            TRAINER.COOP.CSC ${CSC} \
+            TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \
+            DATASET.NUM_SHOTS ${SHOTS}
+        fi
     else
 #        echo
         python train.py \
